360bbf51cc43c84751ac8a91a56e414d10bcc1d9
坚硬的石头 <do_nb@outlook.com>
2017-03-11 16:08:40 +0800

    [4.2] 011-另一种对异常进行测试的方法

--- 4.2/010-ExceptionTest3.php
+++ 4.2/010-ExceptionTest3.php
@@ -1,7 +1,7 @@
 <?php
 
 // 预期被测代码将引发异常
-class ExceptionTest extends PHPUnit_Framework_TestCase
+class ExceptionTest3 extends PHPUnit_Framework_TestCase
 {
     public function testException()
     {
@@ -37,13 +37,13 @@ class ExceptionTest extends PHPUnit_Framework_TestCase
  *
  * There were 3 failures:
  *
- * 1) ExceptionTest::testException
+ * 1) ExceptionTest3::testException
  * Failed asserting that exception of type "InvalidArgumentException" is thrown.
  *
- * 2) ExceptionTest::testExceptionHasRightMessage
+ * 2) ExceptionTest3::testExceptionHasRightMessage
  * Failed asserting that exception message 'Some Message' contains 'Right Message'.
  *
- * 3) ExceptionTest::testExceptionHasRightCode
+ * 3) ExceptionTest3::testExceptionHasRightCode
  * Failed asserting that 10 is equal to expected exception code 20.
  *
  * FAILURES!
